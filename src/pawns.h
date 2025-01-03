7dd65fcf5ad5f82fb225c86f28e6f0bc240ef2fa
Goutham Bhat <245990+goutham@users.noreply.github.com>
2025-01-03 10:35:47 -0800

    Add IsolatedPawns fn

--- src/pawns.h
+++ src/pawns.h
@@ -24,6 +24,18 @@ U64 PassedPawns(const Board& board) {
   return board.BitBoard(PieceOfSide(PAWN, side)) & ~front_span;
 }
 
+template <Side side>
+  requires(side == Side::WHITE || side == Side::BLACK)
+U64 IsolatedPawns(const Board& board) {
+  const U64 pawn_bitboard = board.BitBoard(PieceOfSide(PAWN, side));
+  const U64 front_fill = bitmanip::siderel::FrontFill<side>(pawn_bitboard);
+  const U64 rear_fill = bitmanip::siderel::RearFill<side>(pawn_bitboard);
+  const U64 file_fill = front_fill | rear_fill;
+  const U64 neighbor_files_fill =
+      bitmanip::PushEast(file_fill) | bitmanip::PushWest(file_fill);
+  return pawn_bitboard & ~neighbor_files_fill;
+}
+
 } // namespace pawns
 
 #endif
